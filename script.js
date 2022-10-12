6239056644daddb7a327fda41795a41b86d6d8b4
dang1707vu <dang1707vu@gmail.com>
2022-10-12 16:51:44 +0200

    fix alarm

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ setInterval(() => {
     s = s < 10 ? "0" + s : s;
     currentTime.innerHTML = `${h}:${m}:${s} ${ampm}`;
 
-    if(alarmTime == `${h}:${m} ${ampm}`) {
+    if(alarmTime == `${h}:${m}`) {
         ringtone.play();
         ringtone.loop = true;
     }
